830edfae25ef63af2a8ff709b8414ac1bb9c9870
Emily Koeck <109485996+EmKo453@users.noreply.github.com>
2024-09-22 09:45:07 +1200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,41 @@
+###################
+###   General   ###
+###################
+
+Location_name = "Location 1"
+
+scan_event = "scan_event"
+
+job_table = "jobs"
+
+break_table = "breaks"
+
+employee_table = "employees"
+
+Locations = ["Location 1", "Location 2", "Location 3"]
+
+LocationsAll = ["All Locations"] + Locations
+
+#######################
+###   MQTT Broker   ###
+#######################
+
+mqttBroker= "raspberrypi"
+
+mqtt_port = 1883
+
+########################
+###   SQL Database   ###
+########################
+
+host = "127.0.0.1"
+
+#user="pi"
+user = "root"
+
+#password="coby!!2"
+password = "winston!!2"
+
+database="jobstafftrackingdatabase"
+
+port = 3306
